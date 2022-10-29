accf4dfbd8fe1aec2735fda3eb7fcf27f45230e7
Joseph Nemec <jnemec84@gmail.com>
2022-10-29 11:09:22 -0500

    make nav links dynamic

---
+++ components/NavbarItems.jsx
@@ -0,0 +1,18 @@
+export const NavbarItems = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About Us",
+    url: "/about",
+  },
+  {
+    title: "Our Services",
+    url: "/services",
+  },
+  {
+    title: "Contact Us",
+    url: "/contact",
+  },
+];
